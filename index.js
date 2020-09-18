b1d8d6f2b04a178d5deec5eae0053e251cfa9d19
rsundone <pavel.serhiichuk@gmail.com>
2020-09-18 16:16:06 +0300

    Added index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function createForm(parentElementId, form) {
+    let parentElement = document.getElementById(parentElementId)
+
+    for (let item of form.fields) {
+        let tag = document.createElement(item.tagName)
+
+        Object.keys(item).forEach(key => {
+            if (key !== 'tagName') {
+                tag[key] = item[key]
+            }
+        })
+
+        parentElement.append(tag)
+    }
+
+}
